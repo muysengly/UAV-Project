3635c9ce9147ead5ab5352c8e77e93b01b7fe406
daesolkim <bbpsq0711@gmail.com>
2023-06-28 23:18:43 +0900

    aa

--- private/daesol/algorythmpractice3.py
+++ private/daesol/algorythmpractice3.py
@@ -14,16 +14,67 @@ def makeUAV(xpos,ypos,maxbeam,uavno):
 
     plt.scatter(x=uav_x, y=uav_y, c="green")
     plt.text(x=uav_x - 3.5, y=uav_y - 4, s=uav_number)
+
+def makeBeamCircle(xpos,ypos,maxbeam,color):
+    uav_x_pos = xpos  # x position of uav 0,1,...,GRID_SIZE-1
+    uav_y_pos = ypos  # y position of uav 0,1,...,GRID_SIZE-1
+    uav_x = GRID[1, uav_x_pos, uav_y_pos]
+    uav_y = GRID[0, uav_x_pos, uav_y_pos]
+    uav_z2 = UAV_ALTITUDE  # uav's altitude [meter]
+
     beam_circle = Ellipse(
         xy=(uav_x, uav_y),
         width=maxbeam,
         height=maxbeam,
         angle=0,
         edgecolor="none",
-        facecolor="yellow",
+        facecolor=color,
         alpha=0.2,
     )
     uav_beam = ax.add_patch(beam_circle)
+
+def searchGu():
+    tmp_count=0
+    count=0
+    for k in range(10):
+        if uavx<=gu_x[k]<=uavx+17 and uavy<=gu_y[k]<=uavy+17: #quadrant1
+            print("quadrant1: ",end='')
+            print(k)
+            gu_memory[0][k]=gu_x[k]
+            gu_memory[1][k]=gu_y[k]
+        if uavx-17<=gu_x[k]<=uavx and uavy<=gu_y[k]<=uavy+17: #quadrant2
+            print("quadrant2: ",end='')
+            print(k)
+            gu_memory[0][k]=gu_x[k]
+            gu_memory[1][k]=gu_y[k]
+        if uavx-17<=gu_x[k]<=uavx and uavy-17<=gu_y[k]<=uavy: #quadrant3
+            print("quadrant3: ",end='')
+            print(k)
+            gu_memory[0][k]=gu_x[k]
+            gu_memory[1][k]=gu_y[k]
+        if uavx<=gu_x[k]<=uavx+17 and uavy-17<=gu_y[k]<=uavy: #quadrant4
+            print("quadrant4: ",end='')
+            print(k)
+            gu_memory[0][k]=gu_x[k]
+            gu_memory[1][k]=gu_y[k]
+    print(gu_memory)
+    for k in range(10):
+        if gu_memory[0][k]!=11:
+            if count==0:
+                plt.plot([15,gu_memory[0][k]],[5,gu_memory[1][k]],color="red")
+                tmp_state[tmp_count][0]=gu_memory[0][k]
+                tmp_state[tmp_count][1]=gu_memory[1][k]
+                #print(tmp_state[tmp_count])
+                count+=1
+            else:
+                #print(tmp_state[tmp_count])
+                plt.plot([tmp_state[tmp_count][0],gu_memory[0][k]],[tmp_state[tmp_count][1],gu_memory[1][k]],color="red")
+                tmp_count+=1
+                tmp_state[tmp_count][0]=gu_memory[0][k]
+                tmp_state[tmp_count][1]=gu_memory[1][k]
+                count+=1
+        
+
 
 #intial parameter
 NUM_GU = 10  # number of ground users
@@ -69,60 +120,53 @@ GRID
 
 # generate uav location
 
-uav_x_pos = 5  # x position of uav 0,1,...,GRID_SIZE-1
-uav_y_pos = 5  # y position of uav 0,1,...,GRID_SIZE-1
+uav_x_pos = 1  # x position of uav 0,1,...,GRID_SIZE-1
+uav_y_pos = 0  # y position of uav 0,1,...,GRID_SIZE-1
 
 uav_x = GRID[1, uav_x_pos, uav_y_pos]
 uav_y = GRID[0, uav_x_pos, uav_y_pos]
 uav_z = UAV_ALTITUDE  # uav's altitude [meter]
 
-
 # print
 uav_x, uav_y, uav_z
-
 fig, ax = plt.subplots(figsize=(6, 6))
 
+countUAV=1
+for i in range(1,11,2):
+    if i==1 or i==5 or i==9:
+        for k in range(0,10,2):
+            makeUAV(i,k,MAX_BEAM_DIAMETER,countUAV)
+            if countUAV%2 == 0:
+                makeBeamCircle(i,k,MAX_BEAM_DIAMETER,"yellow")
+            else:
+                makeBeamCircle(i,k,MAX_BEAM_DIAMETER,"orange")
+            if countUAV%5 !=0:
+                plt.plot([(i+0.5)*10,(i+0.5)*10],[(k+0.5)*10,(k+2.5)*10],color="green")
+            elif countUAV==25:
+                continue
+            else:
+                plt.plot([(i+0.5)*10,(i+2.5)*10],[(k+0.5)*10,(k+0.5)*10],color="green")
+            countUAV+=1
+            
+    else:
+        for k in range(8,-2,-2):
+            makeUAV(i,k,MAX_BEAM_DIAMETER,countUAV)
+            if countUAV%2 == 0:
+                makeBeamCircle(i,k,MAX_BEAM_DIAMETER,"yellow")
+            else:
+                makeBeamCircle(i,k,MAX_BEAM_DIAMETER,"orange")
+            if countUAV%5 ==0:    
+                plt.plot([(i+0.5)*10,(i+2.5)*10],[(k+0.5)*10,(k+0.5)*10],color="green")
+            else:
+                plt.plot([(i+0.5)*10,(i+0.5)*10],[(k+0.5)*10,(k-1.5)*10],color="green")
+               # else:
+            countUAV+=1
 
-plt.scatter(x=uav_x, y=uav_y, c="red")
-plt.text(x=uav_x - 3.5, y=uav_y - 4, s=f"UAV")
-beam_circle = Ellipse(
-    xy=(uav_x, uav_y),
-    width=MAX_BEAM_DIAMETER,
-    height=MAX_BEAM_DIAMETER,
-    angle=0,
-    edgecolor="none",
-    facecolor="orange",
-    alpha=0.2,
-)
-uav_beam = ax.add_patch(beam_circle)
 
-#second uav
-"""
-uav_x_pos2 = 0  # x position of uav 0,1,...,GRID_SIZE-1
-uav_y_pos2 = 5  # y position of uav 0,1,...,GRID_SIZE-1
+        
+#makeUAV(1,0,MAX_BEAM_DIAMETER,2)
 
-uav_x2 = GRID[1, uav_x_pos2, uav_y_pos2]
-uav_y2 = GRID[0, uav_x_pos2, uav_y_pos2]
-uav_z2 = UAV_ALTITUDE  # uav's altitude [meter]
 
-plt.scatter(x=uav_x2, y=uav_y2, c="green")
-plt.text(x=uav_x2 - 3.5, y=uav_y2 - 4, s=f"UAV2")
-beam_circle = Ellipse(
-    xy=(uav_x2, uav_y2),
-    width=MAX_BEAM_DIAMETER,
-    height=MAX_BEAM_DIAMETER,
-    angle=0,
-    edgecolor="none",
-    facecolor="yellow",
-    alpha=0.2,
-)
-uav_beam = ax.add_patch(beam_circle)
-"""
-#second uav end
-
-#makeUAV(3,3,MAX_BEAM_DIAMETER,2)
-#makeUAV(4,3,MAX_BEAM_DIAMETER,3)
-#makeUAV(2,6,MAX_BEAM_DIAMETER,4)
 
 for i in range(NUM_GU):
     plt.scatter(x=gu_x[i], y=gu_y[i], c="blue")
@@ -142,9 +186,23 @@ uavx=(uav_x_pos+0.5)*10
 uavy=(uav_y_pos+0.5)*10
 gu_memory=np.ones((2,10)) + 10 # 2 dim. array of 11, size 10x2
 tmp_state=np.ones((10,2)) 
-tmp_count=0
-count=0
 
+searchGu()
+
+
+plt.xlabel("x-axis [m]")
+plt.ylabel("y-axis [m]")
+plt.title("Simulation Environment")
+plt.xticks(np.arange(X_MIN, X_MAX + 1, X_GRID))
+plt.yticks(np.arange(Y_MIN, Y_MAX + 1, Y_GRID))
+plt.xlim(X_MIN, X_MAX)
+plt.ylim(Y_MIN, Y_MAX)
+plt.grid()
+
+plt.show()
+
+
+"""
 #divide into 4 quadrants like coordinate plane and search
 for k in range(10):
     if uavx<=gu_x[k]<=uavx+17 and uavy<=gu_y[k]<=uavy+17: #quadrant1
@@ -174,29 +232,13 @@ for k in range(10):
             plt.plot([55,gu_memory[0][k]],[55,gu_memory[1][k]],color="red")
             tmp_state[tmp_count][0]=gu_memory[0][k]
             tmp_state[tmp_count][1]=gu_memory[1][k]
-            print(tmp_state[tmp_count])
+            #print(tmp_state[tmp_count])
             count+=1
         else:
-            print(tmp_state[tmp_count])
+            #print(tmp_state[tmp_count])
             plt.plot([tmp_state[tmp_count][0],gu_memory[0][k]],[tmp_state[tmp_count][1],gu_memory[1][k]],color="red")
             tmp_count+=1
             tmp_state[tmp_count][0]=gu_memory[0][k]
             tmp_state[tmp_count][1]=gu_memory[1][k]
             count+=1
-            
-
-
-
-
-plt.xlabel("x-axis [m]")
-plt.ylabel("y-axis [m]")
-plt.title("Simulation Environment")
-plt.xticks(np.arange(X_MIN, X_MAX + 1, X_GRID))
-plt.yticks(np.arange(Y_MIN, Y_MAX + 1, Y_GRID))
-plt.xlim(X_MIN, X_MAX)
-plt.ylim(Y_MIN, Y_MAX)
-plt.grid()
-
-plt.show()
-
-
+"""
