30d9cc4f29958bcb518f48bc5d9561d26820c824
daesolkim <bbpsq0711@gmail.com>
2023-06-30 16:25:50 +0900

    aa

--- private/daesol/routeOptimization practice.py
+++ private/daesol/routeOptimization practice.py
@@ -11,25 +11,13 @@ import pandas as pd
 from scipy.spatial import distance_matrix
 
 def makeUAV(xpos,ypos,maxbeam,uavno):
-    uav_x_pos = xpos  # x position of uav 0,1,...,GRID_SIZE-1
-    uav_y_pos = ypos  # y position of uav 0,1,...,GRID_SIZE-1
     uav_number = "UAV"+str(uavno)
-    uav_x = GRID[1, uav_x_pos, uav_y_pos]
-    uav_y = GRID[0, uav_x_pos, uav_y_pos]
-    uav_z2 = UAV_ALTITUDE  # uav's altitude [meter]
-
-    plt.scatter(x=uav_x, y=uav_y, c="green")
-    plt.text(x=uav_x - 3.5, y=uav_y - 4, s=uav_number)
+    plt.scatter(x=xpos, y=ypos, c="green")
+    plt.text(x=xpos - 3.5, y=ypos - 4, s=uav_number)
 
 def makeBeamCircle(xpos,ypos,maxbeam,color):
-    uav_x_pos = xpos  # x position of uav 0,1,...,GRID_SIZE-1
-    uav_y_pos = ypos  # y position of uav 0,1,...,GRID_SIZE-1
-    uav_x = GRID[1, uav_x_pos, uav_y_pos]
-    uav_y = GRID[0, uav_x_pos, uav_y_pos]
-    uav_z2 = UAV_ALTITUDE  # uav's altitude [meter]
-
     beam_circle = Ellipse(
-        xy=(uav_x, uav_y),
+        xy=(xpos, ypos),
         width=maxbeam,
         height=maxbeam,
         angle=0,
@@ -72,7 +60,7 @@ df=pd.read_excel('locationInformation.xlsx')
 print(df.iloc[1,0])
 for x in range(2):
     for y in range(10):
-        gu_memory[x][y]=df.iloc[y+1,x]
+        gu_memory[x][y]=int(df.iloc[y+1,x])
 print(gu_memory)
 
 # generate meshgrid
@@ -81,45 +69,25 @@ tmp_y = np.linspace(Y_MIN + Y_GRID/2,Y_MAX-Y_GRID/2,Y_GRID)
 GRID = np.array(np.meshgrid(tmp_x, tmp_y))
 
 GRID
-
-# generate uav location
-
-uav_x_pos = 1  # x position of uav 0,1,...,GRID_SIZE-1
-uav_y_pos = 0  # y position of uav 0,1,...,GRID_SIZE-1
-
-uav_x = GRID[1, uav_x_pos, uav_y_pos]
-uav_y = GRID[0, uav_x_pos, uav_y_pos]
-uav_z = UAV_ALTITUDE  # uav's altitude [meter]
-
-# print
-uav_x, uav_y, uav_z
 fig, ax = plt.subplots(figsize=(6, 6))
 
 for i in range(NUM_GU):
     plt.scatter(x=gu_memory[0][i], y=gu_memory[1][i], c="blue")
     plt.text(x=gu_memory[0][i] - 3.5, y=gu_memory[1][i] - 4, s=f"GU-{i}")
     plt.text(x=gu_memory[0][i] - 6, y=gu_memory[1][i] - 7, s=f"{gu_bat[i]}mWh")
-makeUAV(9,8,MAX_BEAM_DIAMETER,1)
-makeBeamCircle(9,8,MAX_BEAM_DIAMETER,'orange')
+makeUAV(gu_memory[0][1],gu_memory[1][1],MAX_BEAM_DIAMETER,1)
 
-sxpos=95
-sypos=85
 
-stepCount=1
+stepCount=0
 foundCount=0
 foundUAVno=np.zeros(10)
-for x in range(10):
-    plt.text(x=sxpos,y=sypos,s=str(stepCount))
-    sxpos-=10
-    stepCount+=1
-    for i in range(10):
-        if sxpos-17<=gu_memory[0][i]<=sxpos+17 and sypos-17<=gu_memory[1][i]<=sypos+17:
-            if foundUAVno[i]!=1:
-                foundCount+=1
-            foundUAVno[i]=1
+
+
+
             
 print(foundUAVno)
 print(foundCount)
+print(stepCount)
 
 
 plt.xlabel("x-axis [m]")
@@ -132,3 +100,12 @@ plt.ylim(Y_MIN, Y_MAX)
 plt.grid()
 plt.show()
 
+"""for x in range(9):
+    stepCount+=1
+    #plt.text(x=sxpos,y=sypos,s=str(stepCount))
+    sxpos-=10
+    for i in range(10):
+        if sxpos-17<=gu_memory[0][i]<=sxpos+17 and sypos-17<=gu_memory[1][i]<=sypos+17:
+            if foundUAVno[i]!=1:
+                foundCount+=1
+            foundUAVno[i]=1"""
